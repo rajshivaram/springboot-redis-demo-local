6c413a9ef7fc5f579f98de6a22efed36c89d8523
Raj Shivaram <86063900+rajshivaram@users.noreply.github.com>
2023-04-19 21:36:18 +0530

    Initial Commit

---
+++ src/main/java/com/hspdemo/redis/repository/UserDao.java
@@ -0,0 +1,17 @@
+package com.hspdemo.redis.repository;
+
+import com.hspdemo.redis.model.User;
+
+import java.util.List;
+
+public interface UserDao {
+    boolean saveUser(User user);
+
+    List<User> fetchAllUser();
+
+    User fetchUserById(Long id);
+
+    boolean deleteUser(Long id);
+
+    boolean updateUser(Long id, User user);
+}
---
+++ src/main/java/com/hspdemo/redis/service/UserService.java
@@ -0,0 +1,18 @@
+package com.hspdemo.redis.service;
+
+import com.hspdemo.redis.model.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    boolean saveUser(User user);
+
+    List<User> fetchAllUser();
+
+    User fetchUserById(Long id);
+
+    boolean deleteUser(Long id);
+
+    boolean updateUser(Long id, User user);
+}
